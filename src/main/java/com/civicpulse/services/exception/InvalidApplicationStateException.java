170a1f6aaf721848bcac9ee1c889e242530e035c
MUPPALAM PREMSAI <premsaim23@gmail.com>
2026-07-21 19:37:26 +0530

    Delete src/main/java/com/civicpulse/services directory

--- src/main/java/com/civicpulse/services/exception/InvalidApplicationStateException.java
+++
@@ -1,5 +0,0 @@
-package com.civicpulse.services.exception;
-
-public class InvalidApplicationStateException extends RuntimeException {
-    public InvalidApplicationStateException(String message) { super(message); }
-}
--- src/main/java/com/civicpulse/services/repository/ApplicationDocumentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.civicpulse.services.repository;
-
-import com.civicpulse.services.entity.ApplicationDocument;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplicationDocumentRepository extends JpaRepository<ApplicationDocument, Long> {
-}
